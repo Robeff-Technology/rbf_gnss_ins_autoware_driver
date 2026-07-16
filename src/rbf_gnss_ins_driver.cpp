916dce457f07dee6e0ae43cbd9e54b8308c712f7
kemal <ksoyaltun@gmail.com>
2026-07-16 10:56:08 +0300

    fix: update INS to twist message conversion for body-fixed frame and covariance rotation

--- src/rbf_gnss_ins_driver.cpp
+++ src/rbf_gnss_ins_driver.cpp
@@ -226,10 +226,6 @@ void GnssInsDriver::binary_callback(const uint8_t * data, GnssStreamParser::Mess
         converter_->ins_to_nav_sat_fix_msg(ins_pva_, config_params_.frames_.gnss_frame_);
       pub_nav_sat_fix_->publish(nav_sat_fix_msg);
 
-      auto imu_msg =
-        converter_->ins_to_imu_msg(ins_pva_, raw_imux_, config_params_.frames_.imu_frame_);
-      pub_imu_->publish(imu_msg);
-
       auto twist_msg =
         converter_->ins_to_twist_msg(ins_pva_, raw_imux_, config_params_.frames_.gnss_frame_);
       pub_twist_->publish(twist_msg);
